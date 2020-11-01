f24c36504ad7db02c061445890d25620b3e7dda2
15chouchou <ymmoco.rose@gmail.com>
2020-11-02 08:43:27 +0900

    課題提出ーJavaの基礎

---
+++ Basic/src/Task1.java
@@ -0,0 +1,10 @@
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		//課題１
+	     System.out.println("初めての課題");
+	     System.out.print("Java攻略の道");
+	}
+
+}
---
+++ Basic/src/task2.java
@@ -0,0 +1,11 @@
+
+public class task2 {
+
+	public static void main(String[] args) {
+		//課題２
+		System.out.println("hogehoge");
+		System.out.println("hogehoge2");
+
+	}
+
+}
